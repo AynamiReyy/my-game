9bb3772192d09ad4d5dcf5c15c8bb9bb5e88e7e1
Heiji-Ookubo <dima.smirnow04@gmail.com>
2026-04-23 23:08:35 +0300

    Tail add

--- Teil/obraz/Барабаны.tsx
+++ Teil/obraz/Барабаны.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="Барабаны" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="Барабаны.png" width="128" height="128"/>
+<tileset version="1.10" tiledversion="1.12.1" name="Усилители" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="Декор.png/2.png" width="128" height="128"/>
 </tileset>
